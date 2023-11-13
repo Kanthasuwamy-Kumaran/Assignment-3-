2599792ccbd4907db22815ba136c8de1dca705cb
Kanthasuwamy-Kumaran <rkkumaran834@gmail.com>
2023-11-13 21:02:05 +0530

    reupdate

--- Assets/js/website.js
+++ Assets/js/website.js
@@ -1,4 +1,21 @@
 function myFunction() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("light-mode");
  }
+
+ 
+
+
+function initMap() {
+    var options= {
+        zoom:20,
+        center:{lat:9.657770,lng:80.159210}
+        }
+        var googlemap = new google.maps.Map(document.getElementById("map"), options);
+        }
+
+
+
+        $(document).ready(function() {
+            hyperform(window);
+        });
